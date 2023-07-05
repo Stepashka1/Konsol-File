d00598f5e81a1cbfb05cd90aa8e96243bf3e06ab
Рыкова Мария <RykovaMarii@yandex.ru>
2023-07-05 11:29:25 +0300

    Добавьте файлы проекта.

---
+++ Konsol/IComparable.cs
@@ -0,0 +1,11 @@
+﻿// IComparable.cs Выполнил студент группы 34ИС Родионов Степан
+using System;
+
+namespace RodionovStepan34is.Library
+{
+    public interface IComparable
+    {
+        int CompareTo(object obj);
+    }
+}
+
---
+++ Konsol/Program.cs
@@ -0,0 +1,32 @@
+﻿// Program.cs Выполнил студент группы 34ИС Родионов Степан
+using System;
+using RodionovStepan34is.Library;
+
+namespace RodionovStepan34is.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+            store.Name = "MAGA z";
+            store.Address = "Ylica Bezztijih Fonarei";
+
+            Product product1 = new Product { Name = "Product 1", Number = 5 };
+            Product product2 = new Product { Name = "Product 2", Number = 3 };
+            Product product3 = new Product { Name = "Product 3", Number = 7 };
+
+            store.Products.Add(product1);
+            store.Products.Add(product2);
+            store.Products.Add(product3);
+
+            store.ShowAll();
+
+            Console.WriteLine("\nСортировка по номеру...");
+            store.Products.Sort();
+            store.ShowAll();
+
+            Console.ReadLine();
+        }
+    }
+}
